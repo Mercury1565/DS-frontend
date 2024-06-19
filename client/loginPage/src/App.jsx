a393aba8fee6c73cfdb5c5c8f2f4d1ad6b40e1bb
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-06-19 18:26:23 +0300

    Update App.jsx

--- client/loginPage/src/App.jsx
+++ client/loginPage/src/App.jsx
@@ -1,22 +1,8 @@
 import { useState } from 'react'
-import LoginPage from './components/loginSignup/LoginPage.jsx'
-import AdminPage from './components/Admin/AdminPage.jsx'
-import StudentGrades from './components/Students/StudentGrades.jsx'
-import CourseReg  from './components/Admin/CourseReg.jsx' 
-import StudentPage from './components/Students/StudentPage.jsx'
-import AnnounceExam from './components/Teacher/AnnounceExam.jsx'
-import SubmitGrades from './components/Teacher/SubmitGrades.jsx'
 
 function App() {
 
   return (
-    <>
-    {/* <p>Hello</p> */}
-    {/* <StudentGrades/> */}
-    {/* <StudentPage/> */}
-    {/* <AnnounceExam/> */}
-    <SubmitGrades/>
-    </>
   )
 }
 
