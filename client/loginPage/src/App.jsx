04c90e8f97461c2531f981336d147905d6dfe4f9
mercury1565 <hermongetachew10@gmail.com>
2024-06-19 16:35:01 +0300

    finished AnnounceExam

--- client/loginPage/src/App.jsx
+++ client/loginPage/src/App.jsx
@@ -4,13 +4,15 @@ import AdminPage from './components/Admin/AdminPage.jsx'
 import StudentGrades from './components/Students/StudentGrades.jsx'
 import CourseReg  from './components/Admin/CourseReg.jsx' 
 import StudentPage from './components/Students/StudentPage.jsx'
+import AnnounceExam from './components/Teacher/AnnounceExam.jsx'
 
 function App() {
 
   return (
     <>
-    <StudentGrades/>
+    {/* <StudentGrades/> */}
     {/* <StudentPage/> */}
+    <AnnounceExam/>
     </>
   )
 }
